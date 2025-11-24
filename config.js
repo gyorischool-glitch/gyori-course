561b36a3a45f944107a186d833e40e01a526f984
gyorischool-glitch <gyorischool@gmail.com>
2025-11-24 16:55:20 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,22 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBu7kqKScxk7-8CgsXRi4hCiIYmPHpjeYU",
-  authDomain: "gyori-course.firebaseapp.com",
-  databaseURL: "https://gyori-course-default-rtdb.firebaseio.com",
-  projectId: "gyori-course",
-  storageBucket: "gyori-course.firebasestorage.app",
-  messagingSenderId: "179567019982",
-  appId: "1:179567019982:web:033c80b88d69beb1474d9f",
-  measurementId: "G-EZ1MF7LNPK"
+// Firebase 설정 (v8용)
+var firebaseConfig = {
+    apiKey: "AIzaSyBu7kqKSczk7-8CgsXRi4hCiTmPHpjeYU",
+    authDomain: "gyori-course.firebaseapp.com",
+    databaseURL: "https://gyori-course-default-rtdb.firebaseio.com",
+    projectId: "gyori-course",
+    storageBucket: "gyori-course.appspot.com",
+    messagingSenderId: "1795767019982",
+    appId: "1:1795767019982:web:033c8b088d69beb1474d9f",
+    measurementId: "G-EZ1MF7LNPK"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Firebase 초기화
+firebase.initializeApp(firebaseConfig);
