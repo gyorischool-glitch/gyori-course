0d36d1f1d21e3006446f76d05d6691d97788f34d
gyorischool-glitch <gyorischool@gmail.com>
2025-11-24 16:52:02 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXXXXXXXXXX",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBu7kqKScxk7-8CgsXRi4hCiIYmPHpjeYU",
+  authDomain: "gyori-course.firebaseapp.com",
+  databaseURL: "https://gyori-course-default-rtdb.firebaseio.com",
+  projectId: "gyori-course",
+  storageBucket: "gyori-course.firebasestorage.app",
+  messagingSenderId: "179567019982",
+  appId: "1:179567019982:web:033c80b88d69beb1474d9f",
+  measurementId: "G-EZ1MF7LNPK"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
