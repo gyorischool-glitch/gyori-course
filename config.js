80a6db33ef85fb3854bfc40d4fa784c6f8c28350
gyorischool-glitch <gyorischool@gmail.com>
2025-11-27 13:17:27 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBu7KqKCsczk7-8C9sXRi4hCi1YmPHpjeYU",
+    authDomain: "gyori-course.firebaseapp.com",
+    databaseURL: "https://gyori-course-default-rtdb.firebaseio.com",
+    projectId: "gyori-course",
+    storageBucket: "gyori-course.appspot.com",
+    messagingSenderId: "179567019982",
+    appId: "1:179567019982:web:033c80b88d69beb1474d9f",
+    measurementId: "G-EZ1MF7LNPK"
+};
+firebase.initializeApp(firebaseConfig);
