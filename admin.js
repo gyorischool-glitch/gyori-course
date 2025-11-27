d5c00fd20208abe616c636069769a69ba771e735
gyorischool-glitch <gyorischool@gmail.com>
2025-11-27 13:16:19 +0900

    Delete admin.js

--- admin.js
+++
@@ -1,7 +0,0 @@
-function adminLogin(){
-    const email=document.getElementById('email').value;
-    const pw=document.getElementById('pw').value;
-    firebase.auth().signInWithEmailAndPassword(email,pw)
-    .then(()=>{window.location.href='admin.html';})
-    .catch(e=>alert('로그인 실패: '+e.message));
-}
