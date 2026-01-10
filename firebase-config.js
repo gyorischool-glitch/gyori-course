2f4bd1da1b89b5c2132902ab667f02e3bace4410
gyorischool-glitch <gyorischool@gmail.com>
2026-01-10 22:03:23 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,8 @@
-// firebase-config.js
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js - Firebase v9+ modular setup (CDN)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBu7kqKScxk7-8CgsXRi4hCiIYmPHpjeYU",
   authDomain: "gyori-course.firebaseapp.com",
@@ -18,6 +14,6 @@ const firebaseConfig = {
   measurementId: "G-W862ZZXQY3"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
